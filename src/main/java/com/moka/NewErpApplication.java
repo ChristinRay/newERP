b72df4ce4b74d52ac7492581e1321a8dc501e285
a4191708 <285942003@qq.com>
2018-09-11 10:16:02 +0800

    1
    Signed-off-by: a4191708 <285942003@qq.com>

---
+++ src/main/java/com/moka/NewErpApplication.java
@@ -0,0 +1,35 @@
+package com.moka;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+@SpringBootApplication
+public class NewErpApplication {
+	@Bean
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/newerp?useAffectedRows=true");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		dataSource.setValidationQuery("SELECT 1");
+		dataSource.setTestWhileIdle(true);
+		dataSource.setTimeBetweenEvictionRunsMillis(300000);
+		dataSource.setNumTestsPerEvictionRun(50);
+		dataSource.setMinEvictableIdleTimeMillis(3600000);
+		return dataSource;
+	}
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(getDataSource());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewErpApplication.class, args);
+	}
+}
---
+++ src/main/java/com/moka/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.moka.controller;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* @author    created by lbq
+* @date	     2018年9月10日 下午7:34:33
+**/
+@RestController
+@RequestMapping(value = "/erp/v1/product/")
+@Transactional(rollbackFor = Exception.class)
+public class ProductController {
+	@PostMapping("ceshi")
+	public String ceshi (){
+		return "测试";
+	}
+}
