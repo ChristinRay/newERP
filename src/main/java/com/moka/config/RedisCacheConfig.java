68dba1952ed43403b5ec4c67d65dbd8fa83ff4aa
a4191708 <285942003@qq.com>
2018-09-25 10:11:16 +0800

    redis 配置

--- src/main/java/com/moka/config/RedisCacheConfig.java
+++ src/main/java/com/moka/config/RedisCacheConfig.java
@@ -1,17 +1,12 @@
 package com.moka.config;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
